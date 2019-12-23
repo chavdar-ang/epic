706559f7a4a3212515348d47677434549506e8f4
Chavdar Angelov <chavdar84@gmail.com>
2019-12-23 15:18:34 +0200

    Package lvieira/arangodb-php-odm:^3.5 added.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use ArangoDB\Collection\Collection;
+use ArangoDB\Connection\Connection;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +15,30 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    
+    $connection = new Connection([
+        'host' => 'http://localhost',
+        'port' => 8529,
+        'username' => 'root',
+        'password' => '123456',
+        'database' => '_system',
+        'connection' => 'Keep-Alive',
+        'timeout' => 30
+    ]);
+    
+    $database = $connection->getDatabase();
+    
+    // // If collection exists on database, the object will be a representation of it.
+    $collection = new Collection('users', $database);
+    
+    // // If collection not exists, you can create it with method 'save'
+    $collection->save();
+    
+    // // Get all documents from collection
+    foreach ($collection->all() as $document){
+        dump($document);
+    }
+
+    // return view('welcome');
 });
